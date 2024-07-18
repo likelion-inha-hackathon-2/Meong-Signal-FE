45c054075753795c12b693bcd502c1dade12be6e
abyss-s <xyz@inha.edu>
2024-07-18 18:02:21 +0900

    fix: eslint 유효성 검사 추가

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,36 +1,52 @@
 import React from "react";
 import styled from "styled-components";
-
+import PropTypes from "prop-types";
 
 const InputField = styled.input`
-    width: 100%;
-    height: 40px;
-    padding: 0 10px;
-    border: none;
-    border-radius: 4px;
-    font-size: 14px;
-    background-color: var(--yellow-color1);
-    color: var(--black-color);
-    font-family: ${(props) => props.fontFamily || "pretendard"};
-    &::placeholder {
-        color: var(--gray-color3);
-    }
-    &:focus {
-        outline: none;
-    }
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+  background-color: var(--yellow-color1);
+  color: var(--black-color);
+  font-family: ${(props) => props.fontFamily || "pretendard"};
+  &::placeholder {
+    color: var(--gray-color3);
+  }
+  &:focus {
+    outline: none;
+  }
 `;
 
-const Input = ({ placeholder, type = "text", value, onChange, fontFamily, ...rest }) => {
-    return (
-        <InputField
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        fontFamily={fontFamily}
-        {...rest}
-        />
-    );
+const Input = ({
+  placeholder,
+  type = "text",
+  value,
+  onChange,
+  fontFamily,
+  ...rest
+}) => {
+  return (
+    <InputField
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      fontFamily={fontFamily}
+      {...rest}
+    />
+  );
+};
+
+// eslint 유효성 검사 추가
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func.isRequired,
+  fontFamily: PropTypes.string,
 };
 
 export default Input;
