205f0e7f30ba04b19f4e406f9d3f1c98955f3d6b
abyss-s <xyz@inha.edu>
2024-07-20 01:20:41 +0900

    comment: 콘솔 로그 지우기

--- src/apis/getBoringDogs.js
+++ src/apis/getBoringDogs.js
@@ -4,7 +4,7 @@ import authApi from "./authApi";
 export const getBoringDogs = async () => {
   try {
     const response = await authApi.get("/dogs/boring");
-    console.log("심심한 강아지 목록:", response.data); // 테스트 로그
+    // console.log("심심한 강아지 목록:", response.data); // 테스트 로그
     return response.data.dogs;
   } catch (error) {
     console.error(
