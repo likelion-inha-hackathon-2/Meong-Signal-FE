3f0b1216bef7c6d1c5d7829854a06fd36a6cfd9c
abyss-s <xyz@inha.edu>
2024-07-19 23:49:42 +0900

    refactor: #16 주소 위도 경도 변환 함수를 카카오 제공 api로 변경

--- src/apis/kakaoApi.js
+++ src/apis/kakaoApi.js
@@ -12,13 +12,40 @@ const kakaoApi = axios.create({
 // 집 주소를 보내면 위도, 경도와 함께 변환
 export const getCoordinates = async (roadAddress) => {
   try {
-    const response = await axios.post("/walks/coordinate", {
-      road_address: roadAddress,
+    const response = await kakaoApi.get("/search/address.json", {
+      params: {
+        query: roadAddress,
+      },
     });
-    const { latitude, longitude } = response.data;
-    return { latitude: parseFloat(latitude), longitude: parseFloat(longitude) };
+    const { documents } = response.data;
+    if (documents.length > 0) {
+      const { y: latitude, x: longitude } = documents[0].address;
+      return {
+        latitude: parseFloat(latitude),
+        longitude: parseFloat(longitude),
+      };
+    } else {
+      throw new Error("No coordinates found.");
+    }
   } catch (error) {
     console.error("Error fetching coordinates:", error);
+    throw error;
+  }
+};
+
+// 심심한 상태의 강아지를 조회
+export const getBoringDogs = async () => {
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_BACKEND_URL}/dogs/boring`,
+    );
+    console.log("심심한 강아지 목록:", response.data); // 테스트 로그
+    return response.data.dogs;
+  } catch (error) {
+    console.error(
+      "Error fetching boring dogs:",
+      error.response ? error.response.data : error.message,
+    );
     throw error;
   }
 };
