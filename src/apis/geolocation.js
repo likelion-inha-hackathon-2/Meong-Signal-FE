430a3e2e31235d03557e40de2c40fcf21ebeaea9
영 <77565980+abyss-s@users.noreply.github.com>
2024-07-23 02:26:33 +0900

    refactor: api 파일 모듈화 구조 분리

--- src/apis/geolocation.js
+++ src/apis/geolocation.js
@@ -1,4 +1,4 @@
-// 현재 위치 받아오는 함수(geolocation 기반)
+// 현재 위치 받아오는 함수
 
 export const getCurrentPosition = () => {
   return new Promise((resolve, reject) => {
@@ -16,4 +16,20 @@ export const getCurrentPosition = () => {
       reject(new Error("브라우저에서 현 위치를 받아오는 데 실패했습니다."));
     }
   });
+};
+
+// 현재 위치를 보내서 위도, 경도로 변환
+export const getCoordinates = async () => {
+  try {
+    const position = await getCurrentPosition();
+    const { latitude, longitude } = position;
+
+    return {
+      latitude: parseFloat(latitude),
+      longitude: parseFloat(longitude),
+    };
+  } catch (error) {
+    console.error("Error fetching coordinates:", error);
+    throw error;
+  }
 };
--- src/apis/kakaoApi.js
+++ src/apis/kakaoApi.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { getCurrentPosition } from "./geolocation";
 
 const kakaoApi = axios.create({
   baseURL: "https://dapi.kakao.com/v2/local",
@@ -9,33 +8,5 @@ const kakaoApi = axios.create({
     Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_API_KEY}`,
   },
 });
-
-// 현재 위치를 보내서 위도, 경도로 변환
-export const getCoordinates = async () => {
-  try {
-    const position = await getCurrentPosition();
-    const { latitude, longitude } = position;
-
-    const response = await kakaoApi.get("/geo/coord2address.json", {
-      params: {
-        x: longitude,
-        y: latitude,
-      },
-    });
-
-    const { documents } = response.data;
-    if (documents.length > 0) {
-      return {
-        latitude: parseFloat(latitude),
-        longitude: parseFloat(longitude),
-      };
-    } else {
-      throw new Error("No address found.");
-    }
-  } catch (error) {
-    console.error("Error fetching address:", error);
-    throw error;
-  }
-};
 
 export default kakaoApi;
