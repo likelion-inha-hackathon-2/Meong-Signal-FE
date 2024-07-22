8e003cee46839532699886cb1236b0482c4c4271
영 <77565980+abyss-s@users.noreply.github.com>
2024-07-23 05:32:07 +0900

    fix: 이미지 컴포넌트 돔 에러 수정

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -3,28 +3,23 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const StyledImage = styled.img`
-  width: ${(props) => props.width || "60px"};
-  height: ${(props) => props.height || "60px"};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   border-radius: ${(props) => props.borderRadius || "50%"};
   object-fit: cover;
   cursor: pointer;
   filter: brightness(1);
   transition: filter 0.5s ease;
-
-  &:hover {
-    filter: brightness(0.8); /* Hover 시 밝기를 낮춰줌 */
-  }
 `;
 
-const Image = ({ src, width, height, borderRadius, onClick, ...rest }) => {
+const Image = ({ src, width, height, alt = "default alt text", onClick }) => {
   return (
     <StyledImage
       src={src}
       width={width}
       height={height}
-      borderRadius={borderRadius}
+      alt={alt}
       onClick={onClick}
-      {...rest}
     />
   );
 };
@@ -33,15 +28,8 @@ Image.propTypes = {
   src: PropTypes.string.isRequired,
   width: PropTypes.string,
   height: PropTypes.string,
-  borderRadius: PropTypes.string,
+  alt: PropTypes.string,
   onClick: PropTypes.func,
-};
-
-Image.defaultProps = {
-  width: "60px",
-  height: "60px",
-  borderRadius: "50%",
-  onClick: null,
 };
 
 export default Image;
