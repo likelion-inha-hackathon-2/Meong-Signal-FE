ed41540d2ceb142a440c3ff21a2ac206e701a310
Yoo <tmddls91@inha.edu>
2024-07-27 22:49:26 +0900

    Feat: #42 챌린지 부분 api

---
+++ src/apis/getChallenge.js
@@ -0,0 +1,12 @@
+import authApi from "./authApi";
+
+export const getChallenge = async () => {
+  try {
+    const response = await authApi.get("/walks/challenge");
+    console.log("Response data:", response.data); 
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching challenge data:", error);
+    throw error;
+  }
+};
