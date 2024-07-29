83c9b0b1cb6f99bb2a010f1dce078caf41a211ec
영 <77565980+abyss-s@users.noreply.github.com>
2024-07-30 04:31:47 +0900

    feat: #92 채팅방 읽은 메시지와 안 읽은 메시지 구분하기

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
 
 const ChatRoomContainer = styled.div`
   display: flex;
@@ -9,7 +10,8 @@ const ChatRoomContainer = styled.div`
   padding: 10px;
   margin-bottom: 10px;
   border-radius: 8px;
-  background-color: var(--yellow-color1);
+  background-color: ${(props) =>
+    props.read ? "var(--gray-color1)" : "var(--yellow-color1)"};
   width: 350px;
   font-family: "PretendardM";
   cursor: pointer; /* 클릭 가능하게 커서 변경 */
@@ -26,6 +28,18 @@ const ChatInfo = styled.div`
   display: flex;
   flex-direction: column;
   color: black;
+  flex: 1;
+`;
+
+const ChatHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const NicknameContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Nickname = styled.div`
@@ -33,9 +47,20 @@ const Nickname = styled.div`
   font-size: 18px;
 `;
 
+const RepresentativeBadge = styled.div`
+  background-color: var(--white-color);
+  border-radius: 4px;
+  border: 2px solid var(--yellow-color2);
+  padding: 2px 4px;
+  margin-right: 5px;
+  font-size: 14px;
+  margin-left: 5px;
+`;
+
 const LastMessage = styled.div`
   color: #444444;
-  margin: 5px 0;
+  margin: 10px 0;
+  padding-left: 2px;
 `;
 
 const LastTimeStamp = styled.div`
@@ -44,26 +69,50 @@ const LastTimeStamp = styled.div`
   text-align: right;
 `;
 
+const formatTimestamp = (timestamp) => {
+  if (!timestamp || isNaN(Date.parse(timestamp))) {
+    return "시간 정보 없음";
+  }
+  const now = dayjs();
+  const messageTime = dayjs(timestamp);
+  const diffMinutes = now.diff(messageTime, "minute");
+  const diffHours = now.diff(messageTime, "hour");
+  const diffDays = now.diff(messageTime, "day");
+
+  if (diffMinutes < 60) {
+    return `${diffMinutes}분 전`;
+  } else if (diffHours < 24) {
+    return `${diffHours}시간 전`;
+  } else {
+    return `${diffDays}일 전`;
+  }
+};
+
 const Chat = ({ room }) => {
   const navigate = useNavigate();
-  const lastMessageContent = room.last_message_content || "메시지가 없습니다.";
-  const lastMessageTimestamp =
-    room.last_message_timestamp !== "시간 정보 없음"
-      ? new Date(room.last_message_timestamp).toLocaleString()
-      : "시간 정보 없음";
+  const lastMessageContent =
+    room.last_message_content || "대화 내용이 없습니다.";
+  const lastMessageTimestamp = formatTimestamp(room.last_message_timestamp);
 
   const handleClick = () => {
     navigate(`/chat/rooms/${room.id}`); // room.id로 이동
   };
 
   return (
-    <ChatRoomContainer onClick={handleClick}>
+    <ChatRoomContainer onClick={handleClick} read={room.last_message_read}>
       <ProfileImage
         src={room.other_user_profile_image}
         alt={`${room.other_user_nickname}님의 프로필 사진`}
       />
       <ChatInfo>
-        <Nickname>{room.other_user_nickname}</Nickname>
+        <ChatHeader>
+          <NicknameContainer>
+            {room.other_user_representative && (
+              <RepresentativeBadge>대표</RepresentativeBadge>
+            )}
+            <Nickname>{room.other_user_nickname}</Nickname>
+          </NicknameContainer>
+        </ChatHeader>
         <LastMessage>{lastMessageContent}</LastMessage>
         <LastTimeStamp>{lastMessageTimestamp}</LastTimeStamp>
       </ChatInfo>
@@ -78,6 +127,8 @@ Chat.propTypes = {
     other_user_nickname: PropTypes.string.isRequired,
     last_message_content: PropTypes.string,
     last_message_timestamp: PropTypes.string,
+    last_message_read: PropTypes.bool.isRequired,
+    other_user_representative: PropTypes.bool,
   }).isRequired,
 };
 
